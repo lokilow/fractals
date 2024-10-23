d191fb500b1906cb2801ebe750ff420d14cd0cd9
Chris Lowen <chrislowen@pm.me>
2024-10-23 15:52:28 -0400

    stream png file

--- lib/scenes/home.ex
+++ lib/scenes/home.ex
@@ -3,41 +3,23 @@ defmodule Fractals.Scene.Home do
   require Logger
 
   alias Scenic.Graph
+  alias Scenic.Assets.Stream
 
   import Scenic.Primitives
-  # import Scenic.Components
 
-  @note """
-    This is a very simple starter application.
-
-    If you want a more full-on example, please start from:
-
-    mix scenic.new.example
-  """
-
-  @text_size 24
-
-  # ============================================================================
-  # setup
-
-  # --------------------------------------------------------
   def init(scene, _param, _opts) do
     # get the width and height of the viewport. This is to demonstrate creating
     # a transparent full-screen rectangle to catch user input
     {width, height} = scene.viewport.size
 
-    # show the version of scenic and the glfw driver
-    scenic_ver = Application.spec(:scenic, :vsn) |> to_string()
-    driver_ver = Application.spec(:scenic_driver_local, :vsn) |> to_string()
-
-    info = "scenic: v#{scenic_ver}\nscenic_driver_local: v#{driver_ver}"
+    bin = :code.priv_dir(:fractals) |> Path.join(["mandel.png"]) |> File.read!()
+    {:ok, img} = Stream.Image.from_binary(bin)
+    Stream.put("fractal", img) |> dbg()
 
     graph =
       Graph.build(font: :roboto, font_size: @text_size)
       |> add_specs_to_graph([
-        text_spec(info, translate: {20, 40}),
-        text_spec(@note, translate: {20, 120}),
-        rect_spec({width, height})
+        rect_spec({width, height + 50}, translate: {0, 50}, fill: {:stream, "fractal"})
       ])
 
     scene = push_graph(scene, graph)
