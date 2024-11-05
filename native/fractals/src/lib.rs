392ac3f636c2476a70a6e3f3fedcdd979b575d5c
Chris Lowen <12192034+lokilow@users.noreply.github.com>
2024-11-05 10:47:05 -0500

    parallelize (#4)
    * parallelize
    
    * reorganize lib.rs
    
    * add parallel benchmarks
    
    at first glance, it seems the benchmark is slower with more cores. Maybe this is worth investigating? I wonder if nifs are independent of beam scheduler cores...

--- native/fractals/src/lib.rs
+++ native/fractals/src/lib.rs
@@ -1,17 +1,71 @@
-use image::{GrayImage, Luma};
+extern crate num_cpus;
+use image::GrayImage;
 use num::Complex;
 use rustler::NifMap;
 
-fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
-    let mut z = Complex { re: 0.0, im: 0.0 };
-    for i in 0..limit {
-        // z diverges if its radius is greater than 2
-        if z.norm_sqr() > 4.0 {
-            return Some(i);
+#[derive(NifMap)]
+struct ComplexNifMap {
+    re: f64,
+    im: f64,
+}
+
+impl ComplexNifMap {
+    fn to_complex(&self) -> Complex<f64> {
+        Complex {
+            re: self.re,
+            im: self.im,
         }
-        z = z * z + c
     }
-    None
+}
+
+#[rustler::nif]
+fn generate(
+    image_size: (usize, usize),
+    upper_left_nm: ComplexNifMap,
+    lower_right_nm: ComplexNifMap,
+) -> Vec<u8> {
+    let (width, height) = image_size;
+    let upper_left = upper_left_nm.to_complex();
+    let lower_right = lower_right_nm.to_complex();
+
+    // count logical cores this process could try to use
+    let threads = num_cpus::get();
+    let rows_per_band = height / threads + 1;
+    let total_pixels = width * height;
+    let mut pixels = vec![0; total_pixels];
+
+    let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * width).collect();
+    crossbeam::scope(|spawner| {
+        for (i, band) in bands.into_iter().enumerate() {
+            let top = rows_per_band * i;
+            let band_height = band.len() / width;
+            let band_bounds = (width, band_height);
+
+            let band_upper_left =
+                pixel_to_point((width, height), (0, top), upper_left, lower_right);
+
+            let band_lower_right = pixel_to_point(
+                (width, height),
+                (width, top + band_height),
+                upper_left,
+                lower_right,
+            );
+
+            spawner.spawn(move |_| {
+                render(band, band_bounds, band_upper_left, band_lower_right);
+            });
+        }
+    })
+    .unwrap();
+
+    let mut buf = Vec::new();
+    let encoder = image::codecs::png::PngEncoder::new(&mut buf);
+
+    let img = GrayImage::from_vec(width as u32, height as u32, pixels).unwrap();
+    img.write_with_encoder(encoder)
+        .expect("could not encode png!");
+
+    buf
 }
 
 /// Given the row and column of a  pixel in the output image, return the corresponding point on the complex plane
@@ -52,49 +106,35 @@ fn test_pixel_to_point() {
     )
 }
 
-#[derive(NifMap)]
-struct ComplexNifMap {
-    re: f64,
-    im: f64,
-}
-
-impl ComplexNifMap {
-    fn to_complex(&self) -> Complex<f64> {
-        Complex {
-            re: self.re,
-            im: self.im,
-        }
-    }
-}
-
-#[rustler::nif]
-fn generate(
-    image_size: (usize, usize),
-    upper_left_nm: ComplexNifMap,
-    lower_right_nm: ComplexNifMap,
-) -> Vec<u8> {
-    let (width, height) = image_size;
-    let upper_left = upper_left_nm.to_complex();
-    let lower_right = lower_right_nm.to_complex();
-
-    // render(&mut pixels, bounds, upper_left, lower_right);
-    let mut img = GrayImage::new(width as u32, height as u32);
-
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    let (width, height) = bounds;
     for row in 0..height {
         for col in 0..width {
-            let point = pixel_to_point(image_size, (col, row), upper_left, lower_right);
+            let point = pixel_to_point(bounds, (col, row), upper_left, lower_right);
             let pixel = match escape_time(point, 255) {
                 None => 0,
                 Some(count) => 255 - count as u8,
             };
-            img.put_pixel(col as u32, row as u32, Luma::from([pixel]));
+            pixels[row * width + col] = pixel;
         }
     }
-    let mut buf = Vec::new();
-    let encoder = image::codecs::png::PngEncoder::new(&mut buf);
-    img.write_with_encoder(encoder)
-        .expect("could not encode png!");
-    buf
+}
+
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        // z diverges if its radius is greater than 2
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c
+    }
+    None
 }
 
 rustler::init!("Elixir.Fractals.Generate.Nif");
