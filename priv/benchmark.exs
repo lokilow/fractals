9c15bd5980afedf0d5c629b53a5ea2e793bb0154
Chris Lowen <chrislowen@pm.me>
2024-11-17 10:41:13 -0500

    update benchmark - run for crossbeam

--- priv/benchmark.exs
+++ priv/benchmark.exs
@@ -1,29 +1,24 @@
 require Logger
 function_iterations = 1
 
+bounds = {%{re: -2.0, im: 2.0}, %{re: 2.0, im: -2.0}}
+
 benchmarks = %{
   "mandelbrot" => fn ->
-    for _ <- 0..function_iterations, do: Fractals.Generate.generate({1000, 1000})
+    for _ <- 0..function_iterations,
+        do: Fractals.Generate.generate({1000, 1000}, bounds)
   end
 }
 
-max_cores = :erlang.system_info(:logical_processors_available)
-Logger.info("max cores: #{max_cores}")
+title = "Parallel-Crossbeam"
 
-runs = [1, 2] |> Enum.concat(4..max_cores//4) |> Enum.take_while(&(&1 <= max_cores))
-
-for num_cores <- runs do
-  title = "Parallel - Cores: #{num_cores}"
-
-  Benchee.run(
-    benchmarks,
-    _config = [
-      formatters: [
-        {Benchee.Formatters.JSON, file: "benchmarks/parallel-cores:#{num_cores}.json"},
-        Benchee.Formatters.Console
-      ],
-      title: title,
-      parallel: num_cores
-    ]
-  )
-end
+Benchee.run(
+  benchmarks,
+  _config = [
+    formatters: [
+      {Benchee.Formatters.JSON, file: "benchmarks/#{title}.json"},
+      Benchee.Formatters.Console
+    ],
+    title: title
+  ]
+)
